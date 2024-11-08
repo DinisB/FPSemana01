155110e388fdf51dfb37400c0dcdeccca7143ccb
DinisB <dinisbarroso.dev@gmail.com>
2024-11-08 11:18:30 +0000

    Exercicio 1

--- character.py
+++ character.py
@@ -1,9 +1,25 @@
 chars = []
 count = 3
 for i in range(count):
-    name = input("Name the character")
-    attack = int(input("Insert attack stats"))
-    defense = int(input("Insert defense stats"))
+    name = input()
+    attack = int(input())
+    defense = int(input())
     character =[name, (attack, defense)]
     chars.append(character)
-    print("Nome " + str(chars[i][0]) + " Ataque " + str(chars[i][1][0]) + " Defesa " + str(chars[i][1][1]))
+
+for i in range(count):
+    print(str(chars[i][0]))
+    print(str(chars[i][1][0]))
+    print(str(chars[i][1][1]))
+print(chars)
+max_attack_chars = chars[0]
+max_defense_chars = chars[0]
+
+for character in chars:
+    if character[1][0] > max_attack_chars[1][0]:
+        max_attack_chars = character
+    if character[1][1] > max_defense_chars[1][1]:
+        max_defense_chars = character
+
+print("Max ataque é " + max_attack_chars[0] + " com " + str(max_attack_chars[1][0]))
+print("Max defesa é" + max_defense_chars[0] + " com " + str(max_defense_chars[1][1]))
